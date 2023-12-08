8487953b6f148cd8263201335ff91ce050247413
babita <babita565@gmail.com>
2023-12-08 13:50:51 +0530

    context and local storage

---
+++ src/contexts/ToDoContext.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import { createContext, useContext } from "react";
+
+export const ToDoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo: "Todo messages",
+      completed: false,
+    },
+  ],
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {},
+});
+
+export const useTodo = () => {
+  return useContext(ToDoContext);
+};
+export const TodoProvider = ToDoContext.Provider;
